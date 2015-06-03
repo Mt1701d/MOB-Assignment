5bc7576f1b39685e796e2e1e2a80492559229de5
Michael <mt1701d@gmail.com>
2015-06-03 21:48:19 +0800

    Test Commit

---
+++ MOB-Assignment/Test/Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by MT1701 on 3/6/15.
+//  Copyright (c) 2015 MT1701. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
